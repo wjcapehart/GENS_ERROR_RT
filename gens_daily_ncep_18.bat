f8ea556886779a0e0ae68cc3914e25896cb3d7d8
William.Capehart@sdsmt.edu <wjc@kyrill.ias.sdsmt.edu>
2018-03-16 17:49:44 +0000

    Initial Push

---
+++ gens_daily_ncep_18.bat
@@ -0,0 +1,114 @@
+#/bin/tcsh
+
+ulimit -f unlimited
+
+set HH = 18
+
+set start_date=`date --utc -d "2 day ago" '+%Y-%m-%d'`
+set end_date=`date --utc -d "0 day ago" '+%Y-%m-%d'`
+
+echo $start_date
+echo $end_date
+
+cd /data/NCAR/GENS
+
+
+set file_label = ( ISOHGT MSLP T2M SPCH2M U10 V10 UFLX VFLX FRICV GUST APCP )
+
+set LOCATION_LIST =  (WRFRAP ARMCAR NAMIBI  UCONUS)
+
+
+
+/usr/local/bin/idl   << endidl
+.run /data/NCAR/GENS/gens_daily_ncep_${HH}.pro
+endidl
+
+cd /projects/BIG_WEATHER/GENS_ERROR_RT
+
+
+foreach LOCATION ( $LOCATION_LIST )
+   echo "====================================================="
+   echo "ØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØ"
+   echo
+
+   foreach VARIABLE  ( $file_label )
+
+         echo "====================================================="
+         echo
+
+         echo $LOCATION $VARIABLE  ${start_date}_${HH}
+
+         echo
+         echo "-----------------------------------------------------"
+         echo
+
+         # command-line syntax should read (for example for a fast test case):
+         #  ncl 'file_label="T2M"' 'scenario="WRFRAP"' 'start_date_string="2016-01-01"' 'end_date_string="2016-01-10"'  script_T2M_read_ensembles_from_thredds.ncl
+
+         echo ncl file_label='"'${VARIABLE}'"' \
+                  scenario='"'${LOCATION}'"' \
+                  start_date_string='"'$start_date'"'   \
+                  end_date_string='"'$end_date'"'       \
+                  working_hour='"'$HH'"'       \
+                  script_${VARIABLE}_read_ensembles_from_thredds.ncl
+
+         echo
+         echo "- - - - - - - - - - - - - - - - - - - - - - - - - - -"
+         echo
+
+
+         ncl file_label='"'$VARIABLE'"' \
+             scenario='"'$LOCATION'"' \
+             start_date_string='"'$start_date'"'   \
+             end_date_string='"'$end_date'"'       \
+             working_hour='"'$HH'"'       \
+             script_${VARIABLE}_read_ensembles_from_thredds.ncl
+
+         echo
+         echo "-----------------------------------------------------"
+         echo
+
+	         if [ ${VARIABLE} == "ISOHGT"  ]  && [ ${LOCATION} == "WRFRAP"  ]
+                  then
+
+
+                                     # command-line syntax should read (for example for a fast test case):
+                        #  ncl 'file_label="T2M"' 'scenario="WRFRAP"' 'start_date_string="2016-01-01"' 'end_date_string="2016-01-10"'  script_T2M_read_ensembles_from_thredds.ncl
+
+                        echo ncl file_label='"'${VARIABLE}'"' \
+                                 scenario='"'${LOCATION}'"' \
+                                 start_date_string='"'$start_date'"'   \
+                                 end_date_string='"'$end_date'"'       \
+                                 working_hour='"'$HH'"'       \
+                                 script_plot_triangle_product_ISOHGT.ncl
+
+                        echo
+                        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - -"
+                        echo
+
+
+                        ncl file_label='"'$VARIABLE'"' \
+                            scenario='"'$LOCATION'"' \
+                            start_date_string='"'$start_date'"'   \
+                            end_date_string='"'$end_date'"'       \
+                            working_hour='"'$HH'"'       \
+                            script_plot_triangle_product_ISOHGT.ncl
+
+                        echo
+                        echo "-----------------------------------------------------"
+                        echo
+
+
+                     fi
+   end
+
+
+
+
+
+   echo
+   echo "- - - - - - - - - - - - - - - - - - - - - - - - - - -"
+   echo
+
+
+end
